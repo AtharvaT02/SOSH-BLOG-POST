88ceec8373e3bee891ddcd6e261de24f9f4f364c
Atharva <atharvathorkar2002@gmail.com>
2023-02-08 01:35:51 +0530

    added script file

---
+++ script.py
@@ -0,0 +1,10 @@
+import importlib
+
+def open_brave():
+    try:
+        import webbrowser
+    except ImportError:
+        print("The webbrowser library is not available. Please install it and try again.")
+        return
+
+    webbrowser.get("brave").open("https://www.google.com/")
