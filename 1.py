baea65161615699810f5b37af7f5f70bda1b6fdf
Atharva <atharvathorkar2002@gmail.com>
2023-02-08 04:03:50 +0530

    added 1,py file

--- 1.py
+++ 1.py
@@ -1,12 +1,2 @@
-import importlib
-
-def open_brave():
-    try:
-        import webbrowser
-    except ImportError:
-        print("The webbrowser library is not available. Please install it and try again.")
-        return
-
-    webbrowser.get("brave").open("https://www.google.com/")
-
-open_brave()
+while True:
+    print("Hello, World!")
